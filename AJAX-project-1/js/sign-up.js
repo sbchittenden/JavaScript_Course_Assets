34d56bfaae585827c14d2b08842d447e78bc4ce1
Sam Chittenden <sbchittenden@gmail.com>
2017-09-05 12:08:42 -0600

    updated AJAX project-1
    - was missing cookie greeting functionality

--- AJAX-project-1/js/sign-up.js
+++ AJAX-project-1/js/sign-up.js
@@ -1,31 +1,53 @@
 (function() {
+  'use strict';
   const form = document.getElementById('sign_up_form');
   const firstName = document.getElementById('first_name');
   const lastName = document.getElementById('last_name');
   const email = document.getElementById('email');
   const dob = document.getElementById('date_of_birth');
   const psw = document.getElementById('password');
-  const submit = document.getElementById('submit');
+  const greeting = document.querySelector('.greeting');
   const successDiv = document.querySelector('.success_message');
+  window.onload = function() {
+    if (!document.cookie) {
+      form.addEventListener('submit', function(e) {
+        const xhr = new XMLHttpRequest();
+        const formData = new FormData();
 
-  form.addEventListener('submit', function(e) {
-    const xhr = new XMLHttpRequest();
-    const formData = new FormData();
+        formData.append('firstName', firstName.value);
+        formData.append('lastName', lastName.value);
+        formData.append('email', email.value);
+        formData.append('DOB', dob.value);
+        formData.append('password', psw.value);
 
-    formData.append('firstName', firstName.value);
-    formData.append('lastName', lastName.value);
-    formData.append('email', email.value);
-    formData.append('DOB', dob.value);
-    formData.append('password', psw.value);
+        xhr.onload = function() {
+          form.classList.add('submitted');
+          successDiv.innerHTML = JSON.parse(xhr.response).data;
+          document.cookie = `name=${firstName.value}`;
+          console.log(document.cookie);
+        };
 
-    xhr.onload = function() {
-      successDiv.innerHTML = JSON.parse(xhr.response).data;
-    };
+        xhr.open('POST', '/create_account');
+        xhr.send(formData);
 
-    xhr.open('POST', '/create_account');
-    xhr.send(formData);
+        e.preventDefault();
+      });
+    } else {
+      form.classList.add('is-hidden');
+      let user = getCookies();
+      greeting.innerHTML = `<h2>Welcome back ${user.name}!</h2>`
+    }
+  }
 
-    e.preventDefault();
-  });
+  function getCookies() {
+    var cookieArray = document.cookie.split(';');
+    var cookieObj = {};
+    cookieArray.forEach(function(item) {
+      var key = item.substring(0, item.indexOf('='));
+      var value = item.substring(item.indexOf('=') + 1);
+      cookieObj[key] = value;
+    });
+    return cookieObj;
+  }
+
 })();
-
